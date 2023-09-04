5d7d2191a5665bd842977abbe1291a0a0cd4f1c5
ApinHovo <apinyan.hovhannes@gmail.com>
2023-09-04 18:20:32 +0400

    Added logging in main.py

--- AllInOne/main.py
+++ AllInOne/main.py
@@ -1,8 +1,12 @@
 import time
+import logging
 from db import Database
 from Sensors import AirQualitySensor, CO2Sensor, LightSensor, TPHSensor
 from WeatherMeterSensors import WeatherSensors
 from datetime import datetime
+
+
+logging.basicConfig(filename='parsing.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def read_sensor_data(sensors):
@@ -40,31 +44,41 @@ def get_averages(data, weather):
     return result_data
 
 
-def main(light_obj, tph_obj, air_quality_obj, co2_obj, weather):
-    sensors = {
-        light_obj: "Light",
-        tph_obj: "",
-        air_quality_obj: "",
-        co2_obj: "CO2",
-        weather: ""
-    }
+def main(measuring_time, light_obj, tph_obj, air_quality_obj, co2_obj, weather):
 
-    data_all = {}
+    try:
+        logging.info("Starting data collection.")
+        
+        sensors = {
+            light_obj: "Light",
+            tph_obj: "",
+            air_quality_obj: "",
+            co2_obj: "CO2",
+            weather: ""
+        }
 
-    start_time = time.time()
+        data_all = {}
 
-    while time.time() - start_time < 60:
-        data = read_sensor_data(sensors)
+        start_time = time.time()
 
-        data_all = add_data(data_all, data)
-          
-        remaining_time = 60 - (time.time() - start_time)
-        if remaining_time <= 25:
-            time.sleep(remaining_time)
+        while time.time() - start_time < measuring_time:
+            data = read_sensor_data(sensors)
+            data_all = add_data(data_all, data)
+            
+            remaining_time = measuring_time - (time.time() - start_time)
+            if remaining_time <= 30:
+                time.sleep(remaining_time)
 
-    data_all["direction"] = data_all.get("direction")
+        data_all["direction"] = data_all.get("direction")
 
-    return data_all
+        logging.info("Data collection completed.")
+
+        return data_all
+        
+    except Exception as e:
+        logging.error(f"Error occurred during execution: {str(e)}", exc_info=True)
+
+    
 
 
 if __name__ == "__main__":
@@ -76,13 +90,16 @@ if __name__ == "__main__":
     db = Database()
 
     while True:
-        data = main(light_obj, tph_obj, air_quality_obj, co2_obj, weather)
+        try:
+            data = main(900, light_obj, tph_obj, air_quality_obj, co2_obj, weather)
 
-        data_avg = get_averages(data, weather)
+            data_avg = get_averages(data, weather)
 
-        insert_data = list(data_avg.values())
-        insert_data.insert(0, datetime.now())
+            insert_data = list(data_avg.values())
+            insert_data.insert(0, datetime.now())
 
-        db.insert_data(insert_data)
+            db.insert_data(insert_data)
 
-        print("\n" + ("#" * 50) + "\n")
+            logging.info("Data successfully inserted into the database.")
+        except Exception as e:
+            logging.error(f"Error occurred during execution: {str(e)}", exc_info=True)
