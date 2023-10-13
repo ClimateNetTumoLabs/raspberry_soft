144df2c1be74e1a61c74011735a60d284255843c
ApinHovo <apinyan.hovhannes@gmail.com>
2023-10-13 16:50:05 +0400

    Changes in read_sensor ile

--- AllInOne/read_sensors.py
+++ AllInOne/read_sensors.py
@@ -5,7 +5,7 @@ from logger_config import *
 
 
 class ReadSensor:
-    def __init__(self, measuring_time=900, max_reading_time=30):
+    def __init__(self, measuring_time=900, max_reading_time=15):
         self.__create_sensor_objects()
 
         self.wind_direction_sensor = WindDirection()
@@ -40,10 +40,9 @@ class ReadSensor:
             setattr(self, names["variable_name"], sensor())
             self.sensor_name_mapping[self.__dict__[names["variable_name"]]] = names["name"]
 
-    def __get_data(self):
-        time.sleep(self.MEASURING_TIME - self.MAX_READING_TIME)
-        
+    def __get_data(self, start_time):
         data = {}
+
         for sensor, name in self.sensor_name_mapping.items():
             res = sensor.read_data()
             if isinstance(res, dict):
@@ -51,30 +50,25 @@ class ReadSensor:
             else:
                 data[name] = res
 
-        data["speed"] = 0.0
+        data["speed"] = self.wind_speed_sensor.read_data(time.time() - start_time)
         data["rain"] = 0.0
-        data["direction"] = None
+        data["direction"] = self.wind_direction_sensor.read_data() if data["speed"] != 0 else None
         return data
 
     def get_data(self):
         try:
             self.wind_speed_sensor.clear_data()
             self.rain_sensor.clear_data()
-
-            logging.info(f"{'+' * 10} Starting data collection.")
-            
+            logging.info(f"{'+' * 15} Starting data collection.")
             start_time = time.time()
-            collected_data = self.__get_data()
-            
-            if self.wind_speed_sensor.count != 0:
-                collected_data["direction"] = self.wind_direction_sensor.read_data()
+
+            time.sleep(self.MEASURING_TIME - self.MAX_READING_TIME)
+            collected_data = self.__get_data(start_time)
 
             remaining_time = self.MEASURING_TIME - (time.time() - start_time)
             time.sleep(remaining_time)
 
-            collected_data["speed"] = self.wind_speed_sensor.read_data(time.time() - start_time)
             collected_data["rain"] = self.rain_sensor.read_data()
-
             return collected_data
 
         except Exception as e:
