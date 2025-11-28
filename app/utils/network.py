bff55988063b8f1a212be17af6aab59b16fdcef9
sonatumoarzumanyan <sona.arzumanyan@tumo.org>
2025-11-28 17:27:57 +0400

    previous commit

--- app/utils/network.py
+++ app/utils/network.py
@@ -13,7 +13,7 @@ def check_internet(host="8.8.8.8", port=53, timeout=5):
         return False
 
 
-def reconnect(ssid="tumoTeam", password="hello2team"):
+def reconnect(ssid="MyWiFi", password="mypassword"):
     """Try to reconnect to the internet via LAN or Wi-Fi."""
     logging.info("[Network] Checking connection...")
 
